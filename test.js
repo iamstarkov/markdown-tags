6b32c40b2a35503a1e780f5a00c9b0f07bbe2977
Vladimir Starkov <iamstarkov@gmail.com>
2015-11-05 17:19:50 +0100

    specify tests

--- test.js
+++ test.js
@@ -1,8 +1,23 @@
 import { equal } from 'assert';
 import markdownTags from './index';
 
-it('should markdownTags', () =>
-  equal(markdownTags('unicorns'), 'unicorns'));
+const input = `
+# Title
+
+_November 5, 2015_
+
+#nodejs, #markdown
+
+Description text`;
+
+it('should markdownTags tags', () =>
+  equal(markdownTags(input).tags, ['nodejs', 'markdown']));
+
+it('should markdownTags md', () =>
+  equal(markdownTags(input, text => `YO ${text}`).md, 'YO #nodejs, #markdown'));
+
+it('should markdownTags md', () =>
+  equal(markdownTags(input, text => `YO ${text}`).html, '<p>YO #nodejs, #markdown</p>'));
 
 it('should markdownTags invalid input', () =>
   equal(markdownTags(), undefined));
